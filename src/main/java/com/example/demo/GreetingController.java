df9d2de338b578152e608938e58e414286db47f5
Fouedtrabelsi <fouedtrabelsu2002@gmail.com>
2025-02-17 23:00:49 +0100

    ajout de websocket

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting (Hello greeting) {
+        return new Greeting("hello"+ HtmlUtils.htmlEscape(greeting.getName()));
+    }
+}
